c2517233d9c712394b799a1844a5fcf53c81a98d
Jokotagba Opemipo <opemipojokotagba@gmail.com>
2020-12-02 18:06:47 +0100

    added POST method

--- index.js
+++ index.js
@@ -12,16 +12,33 @@ const genres = [
     {id:5, name: "Frightful"}
 ];
 
+//GET ALL GENRES
 app.get('/api/genres', (req,res) => {
     res.send(genres);
 });
 
+//GET SPECIFIC GENRES
 app.get('/api/genres/:id', (req,res) => {
     const genre = genres.find((c) => c.id === parseInt(req.params.id));
     if (!genre){
         return res.status(404).send("The genre with the given ID does not exist")};
     res.send(genre);
 });
+
+//CREATE A NEW GENRE && USE EXPRESS VALIDATOR FOR VALIDATION
+app.post("/api/genres", [body("name").isLength({ min: 5 })], (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    const genre = {
+      id: genres.length + 1,
+      name: req.body.name,
+    };
+    genres.push(genre);
+    res.send(genre);
+  });
+
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`listening on port ${port}`));
