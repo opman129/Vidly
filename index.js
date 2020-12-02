bd888538bde08e3b4b0270521df2ece01d1cf6a3
Jokotagba Opemipo <opemipojokotagba@gmail.com>
2020-12-02 18:16:56 +0100

    added PATCH method

--- index.js
+++ index.js
@@ -39,6 +39,22 @@ app.post("/api/genres", [body("name").isLength({ min: 5 })], (req, res) => {
     res.send(genre);
   });
 
+app.patch("api/genres/:id",  [body("name").isLength({ min: 5 })], (req,res) => {
+    //GET COURSE USING COURSE ID
+    const genre = genres.find((c) => c.id === parseInt(req.params.id));
+    if (!genre) {
+      return res.status(404).send("The genre with the given ID does not exist");
+    }
+    //VALIDATE COURSE
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    //UPDATE COURSE
+    genre.name = req.body.name;
+    res.send(genre);
+});
+
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`listening on port ${port}`));
