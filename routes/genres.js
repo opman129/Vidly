2cbda9fe6e8677063c6f96bc8927cbdf18a92a31
Jokotagba Opemipo <opemipojokotagba@gmail.com>
2020-12-09 17:06:50 +0100

    initiated db models

--- routes/genres.js
+++ routes/genres.js
@@ -27,17 +27,8 @@ router.get('/', async (req,res) => {
     res.send(genres);
 });
 
-//GET SPECIFIC GENRES
-router.get('/:id', async (req,res) => {
-  const genre = await Movie.find((c) => c.id === parseInt(req.params.id));
-    // const genre = genres.find((c) => c.id === parseInt(req.params.id));
-    if (!genre){
-        return res.status(404).send("The genre with the given ID does not exist")};
-    res.send(genre);
-});
-
 //CREATE A NEW GENRE && USE EXPRESS VALIDATOR FOR VALIDATION
-router.post("/", [body("name").isLength({ min: 5 })], (req, res) => {
+router.post("/", [body("name").isLength({ min: 5 })], async (req, res) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
       return res.status(400).json({ errors: errors.array() });
@@ -48,31 +39,37 @@ router.post("/", [body("name").isLength({ min: 5 })], (req, res) => {
       tags: req.body.tags,
       nowShowing: req.body.nowShowing
     });
-    genre = genre.save();
+    genre = await genre.save();
     res.send(genre);
   });
 
-//UPDATE A SPECIFIC GENRE WITH THE PUT METHOD
-router.patch('/:id',  [body("name").isLength({ min: 5 })], (req,res) => {
-    //GET COURSE USING COURSE ID
-  Movie.findByIdAndUpdate(req.params.id, {
+  //GET SPECIFIC GENRES
+router.get("/:id", async (req, res) => {
+  const genre = await Movie.findById(req.params.id,);
+  if (!genre) {
+    return res.status(404).send("The genre with the given ID does not exist");
+  }
+  res.send(genre);
+});
+
+//UPDATE A SPECIFIC GENRE WITH THE PATCH METHOD
+router.patch('/:id',  [body("name").isLength({ min: 5 })], async (req,res) => {
+  //VALIDATE COURSE
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ errors: errors.array() });
+  }
+  //GET COURSE USING COURSE ID
+  const genre = await Movie.findByIdAndUpdate(req.params.id, {
     name: req.body.name,
     producer: req.body.producer,
     tags: req.body.tags,
   }, { new: true });
 
-
-    const genre = genres.find((c) => c.id === parseInt(req.params.id));
     if (!genre) {
       return res.status(404).send("The genre with the given ID does not exist");
     }
-    //VALIDATE COURSE
-    const errors = validationResult(req);
-    if (!errors.isEmpty()) {
-      return res.status(400).json({ errors: errors.array() });
-    }
-    //UPDATE COURSE
-    genre.name = req.body.name;
+
     res.send(genre);
 });
 
