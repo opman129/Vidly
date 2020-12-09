593116eb1a519a28c27f8ee5f61be6db20a443b1
Jokotagba Opemipo <opemipojokotagba@gmail.com>
2020-12-09 18:13:12 +0100

    added new routes

--- routes/customers.js
+++ routes/customers.js
@@ -13,4 +13,41 @@ const customerSchema = new mongoose.Schema({
   phone: String,
   isGold: Boolean,
   date: { type: Date, default: Date.now },
-});    
+});
+//Model For Customers
+const Customer = mongoose.model("Customer", customerSchema);
+
+//Get all Customers
+router.get("/", async (req,res) => {
+    const customers = await Customer.find().sort('name');
+    res.send(customers);
+});
+
+//Create new Customer
+router.post("/", [body("name").isLength({ min: 5 }), body("phone").isLength({ max: 11 })], async (req, res) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ errors: errors.array() });
+  }
+  let customer = new Customer ({
+    name: req.body.name,
+    phone: req.body.phone,
+    isGold: req.body.isGold
+  });
+  customer = await customer.save();
+  res.send(genre);
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+module.exports = router;
