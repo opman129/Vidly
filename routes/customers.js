49c528b26c37e6a8509196ca788982d2162e3fc8
Jokotagba Opemipo <opemipojokotagba@gmail.com>
2020-12-09 18:22:15 +0100

    added customer api

--- routes/customers.js
+++ routes/customers.js
@@ -35,19 +35,45 @@ router.post("/", [body("name").isLength({ min: 5 }), body("phone").isLength({ ma
     isGold: req.body.isGold
   });
   customer = await customer.save();
-  res.send(genre);
+  res.send(customer);
 });
 
+//Get specific customer
+router.get("/:id", async (req, res) => {
+  const customer = await Customer.findById(req.params.id, { useFindAndModify: false });
+  if (!customer) {
+    return res.status(404).send("The customer with the given ID does not exist");
+  }
+  res.send(customer);
+});
 
+//Update Customer
+router.patch('/:id',  [body("name").isLength({ min: 5 }), body("phone").isLength({ max: 11 })], async (req,res) => {
+  //Validate
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ errors: errors.array() });
+  }
+  //Get Customer by Id
+  const customer = await Customer.findByIdAndUpdate(req.params.id, {
+    name: req.body.name,
+    phone: req.body.phone,
+    isGold: req.body.isGold},
+    { new: true });
+    //Check If customer exists or not
+    if (!customer) {
+      return res.status(404).send("The customer with the given ID does not exist");
+    };
+    res.send(customer);
+});
 
-
-
-
-
-
-
-
-
-
+//Delete Customer
+router.delete('/:id', async (req,res) => {
+  const customer = await Movie.findByIdAndRemove(req.params.id, { useFindAndModify: false });
+  if (!customer){
+      return res.status(404).send("The customer with the given ID does not exist")};
+  //Return Customer
+  res.send(customer);
+});
 
 module.exports = router;
