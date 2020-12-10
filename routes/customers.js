cc9d546c1231cc1b39c3c513093740db628707c6
Jokotagba Opemipo <opemipojokotagba@gmail.com>
2020-12-10 11:56:42 +0100

    added models

--- routes/customers.js
+++ routes/customers.js
@@ -1,4 +1,4 @@
-const {Customer} = require('../models/customer');
+const Customer = require('../models/customer');
 const express = require('express');
 const router = express.Router();
 const { body, validationResult } = require('express-validator');
