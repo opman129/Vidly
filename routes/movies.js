5bfadb96a049343401f940f18b2e1865380e6c20
Jokotagba Opemipo <opemipojokotagba@gmail.com>
2020-12-11 13:39:36 +0100

    added genre collection

---
+++ routes/movies.js
@@ -0,0 +1,66 @@
+const Movie = require('../models/movie');
+const express = require('express');
+const router = express.Router();
+const { body, validationResult } = require('express-validator');
+
+//GET ALL MOVIES
+router.get('/', async (req,res) => {
+    const movies = await Movie.find().sort('name');
+    res.send(movies);
+});
+
+//CREATE A NEW GENRE && USE EXPRESS VALIDATOR FOR VALIDATION
+//Valdate
+router.post("/", [body("title").isLength({ min: 5 })], async (req, res) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ errors: errors.array() });
+  }
+  let movie = new Movie({
+    title: req.body.name,
+    numberInStock: req.body.numberInStock,
+    dailyRentalRate: req.body.dailyRentalRate
+  });
+  movie = await movie.save();
+  res.send(movie);
+});
+
+//GET SPECIFIC GENRES
+router.get("/:id", async (req, res) => {
+  const movie = await Movie.findById(req.params.id, { useFindAndModify: false });
+  if (!movie) {
+    return res.status(404).send("The genre with the given ID does not exist");
+  }
+  res.send(movie);
+});
+
+//UPDATE A SPECIFIC GENRE WITH THE PATCH METHOD
+router.patch('/:id',  [body("name").isLength({ min: 5 })], async (req,res) => {
+  //VALIDATE Movie
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ errors: errors.array() });
+  }
+  //GET Movie USING Movie ID and Update
+  const movie = await Movie.findByIdAndUpdate(req.params.id, {
+    title: req.body.title,
+    numberInStock: req.body.numberInStock,
+    dailyRentalRate: req.body.dailyRentalRate }, 
+    { new: true });
+    //Return Status Code If Genre Doesnt Exist
+    if (!movie) {
+      return res.status(404).send("The genre with the given ID does not exist");
+    };
+    res.send(movie);
+});
+
+//DELETE A SPECIFIC Movie
+router.delete('/:id', async (req,res) => {
+    const movie = await Movie.findByIdAndRemove(req.params.id, { useFindAndModify: false });
+    if (!movie){
+        return res.status(404).send("The genre with the given ID does not exist")};
+    //Return Movie
+    res.send(movie);
+});
+
+module.exports = router;
