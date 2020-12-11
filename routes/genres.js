4e1abecdaa98db3c5f77177c621342035ce7917e
Jokotagba Opemipo <opemipojokotagba@gmail.com>
2020-12-11 13:08:18 +0100

    restructuring

--- routes/genres.js
+++ routes/genres.js
@@ -10,20 +10,21 @@ router.get('/', async (req,res) => {
 });
 
 //CREATE A NEW GENRE && USE EXPRESS VALIDATOR FOR VALIDATION
+//Valdate
 router.post("/", [body("name").isLength({ min: 5 })], async (req, res) => {
-    const errors = validationResult(req);
-    if (!errors.isEmpty()) {
-      return res.status(400).json({ errors: errors.array() });
-    };
-    let genre = new Genre ({
-      name: req.body.name,
-      producer: req.body.producer,
-      tags: req.body.tags,
-      nowShowing: req.body.nowShowing
-    });
-    genre = await genre.save();
-    res.send(genre);
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ errors: errors.array() });
+  }
+  let genre = new Genre({
+    name: req.body.name,
+    producer: req.body.producer,
+    tags: req.body.tags,
+    nowShowing: req.body.nowShowing
   });
+  genre = await genre.save();
+  res.send(genre);
+});
 
   //GET SPECIFIC GENRES
 router.get("/:id", async (req, res) => {
@@ -46,8 +47,8 @@ router.patch('/:id',  [body("name").isLength({ min: 5 })], async (req,res) => {
     name: req.body.name,
     producer: req.body.producer,
     tags: req.body.tags,
-    nowShowing: req.body.nowShowing
-    }, { new: true }, { useFindAndModify: false } );
+    nowShowing: req.body.nowShowing}, 
+    { new: true });
     //return status code if genre doesnt exist
     if (!genre) {
       return res.status(404).send("The genre with the given ID does not exist");
