8130f0c856414e5d3590e98c7d228ba055370a33
Jokotagba Opemipo <opemipojokotagba@gmail.com>
2020-12-12 18:16:04 +0100

    Added delete route

--- routes/movies.js
+++ routes/movies.js
@@ -49,28 +49,34 @@ router.get("/:id", async (req, res) => {
 });
 
 //UPDATE A SPECIFIC MOVIE WITH THE PATCH METHOD
-router.patch('/:id',  [body("title").isLength({ min: 5 })], async (req,res) => {
+router.patch('/:id', [body("title").isLength({ min: 4 })], async (req,res) => {
   //VALIDATE Movie
   const errors = validationResult(req);
   if (!errors.isEmpty()) {
     return res.status(400).json({ errors: errors.array() });
   }
   //GET Movie USING Movie ID and Update
-  const movie = await Movie.findByIdAndUpdate(
-    req.params.id,
-    {
-      title: req.body.title,
-      genre: req.body.genre,
-      numberInStock: req.body.numberInStock,
-      dailyRentalRate: req.body.dailyRentalRate,
-    },
-    { new: true }, { useFindAndModify: false }
-  );
+  try {
+    const movie = await Movie.findByIdAndUpdate(
+      req.params.id,
+      {
+        title: req.body.title,
+        genre: req.body.genre,
+        numberInStock: req.body.numberInStock,
+        dailyRentalRate: req.body.dailyRentalRate,
+      },
+      { new: true },
+      { useFindAndModify: false }
+    );
     //Return Status Code If Movie Doesnt Exist
     if (!movie) {
       return res.status(404).send("The movie with the given ID does not exist");
-    };
+    }
     res.send(movie);
+  }
+    catch(error){
+        console.log(error.message);
+    };
 });
 
 //DELETE A SPECIFIC MOVIE
