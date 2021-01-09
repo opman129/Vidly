e0fee0303cbb0b2e6616bba48a327f035bc7497f
Jokotagba Opemipo <opemipojokotagba@gmail.com>
2021-01-09 20:27:40 +0100

    tests

--- tests/lib.test.js
+++ tests/lib.test.js
@@ -11,6 +11,6 @@ test('absolute - should return a positive number if input is negative', () => {
 });
 
 test('absolute - should return 0 if input is 0', () => {
-    const result = lib.absolute(1);
-    expect(result).toBe(1);
+    const result = lib.absolute(0);
+    expect(result).toBe(0);
 });
