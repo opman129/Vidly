5fe77c0bd840729eaca2edcbca03b0057ad57657
Jokotagba Opemipo <opemipojokotagba@gmail.com>
2021-01-09 08:18:30 +0100

    reroutes

--- routes/genres.js
+++ routes/genres.js
@@ -41,7 +41,7 @@ router.get("/:id", async (req, res) => {
 });
 
 //UPDATE A SPECIFIC GENRE WITH THE PATCH METHOD
-router.patch('/:id',  [body("name").isLength({ min: 5 })], async (req,res) => {
+router.patch("/:id",  [body("name").isLength({ min: 5 })], async (req, res) => {
   //VALIDATE Movie
   const errors = validationResult(req);
   if (!errors.isEmpty()) {
@@ -49,21 +49,25 @@ router.patch('/:id',  [body("name").isLength({ min: 5 })], async (req,res) => {
   }
   //GET Movie USING Movie ID and Update
   try {
-    const genre = await Genre.findByIdAndUpdate(req.params.id, {
-      name: req.body.name,
-      producer: req.body.producer,
-      tags: req.body.tags,
-      nowShowing: req.body.nowShowing}, 
-      { new: true });
-      //Return Status Code If Genre Doesnt Exist
-      if (!genre) {
-        return res.status(404).send("The genre with the given ID does not exist");
-      };
-      res.send(genre);
+    const genre = await Genre.findByIdAndUpdate(
+      req.params.id,
+      {
+        name: req.body.name,
+        producer: req.body.producer,
+        tags: req.body.tags,
+        nowShowing: req.body.nowShowing,
+      },
+      { new: true },
+      { useFindAndModify: false }
+    );
+    //Return Status Code If Genre Doesnt Exist
+    if (!genre) {
+      return res.status(404).send("The genre with the given ID does not exist");
+    }
+    res.send(genre);
   } catch (err) {
-    console.log('Error', err.message);
-  };
-  
+    console.log("Error", err.message);
+  }
 });
 
 //DELETE A SPECIFIC GENRE
