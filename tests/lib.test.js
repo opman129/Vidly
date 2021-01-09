8e3161be2af32dac425fd9a226186c070b77b5ec
Jokotagba Opemipo <opemipojokotagba@gmail.com>
2021-01-09 20:11:22 +0100

    tests

--- tests/lib.test.js
+++ tests/lib.test.js
@@ -1,3 +1,6 @@
-test('', () => {
-    
-})
+const lib = require("./lib");
+
+test('absolute - should return a positive number if input is postive', () => {
+    const result = lib.absolute(1);
+    expect(result).toBe(1);
+});
