29888c93cd859d90817684a90483af0ec35590c5
Jokotagba Opemipo <opemipojokotagba@gmail.com>
2021-01-12 13:42:49 +0100

    tests

--- tests/lib.test.js
+++ tests/lib.test.js
@@ -1,6 +1,7 @@
 const { DESCRIBE } = require("sequelize/types/lib/query-types");
 const lib = require("./lib");
 
+//Always group related tests inside a describe block
 describe("absolute", () => {
   test("should return a positive number if input is postive", () => {
     const result = lib.absolute(1);
