7458631ba2d53da3b31b169e8e027c29340765b5
Jokotagba Opemipo <opemipojokotagba@gmail.com>
2021-01-12 23:38:21 +0100

    Test implementation

--- tests/lib.js
+++ tests/lib.js
@@ -1,5 +1,10 @@
 
-//Testing numbers
-module.exports.absolute = function(number){
-    return (number >= 0 ) ? number : -number;
+// //Testing numbers
+// module.exports.absolute = function(number){
+//     return (number >= 0 ) ? number : -number;
+// }
+
+//Testing Strings
+module.exports.greet = function(name) {
+    return 'Welcome' + name;
 }
--- tests/lib.test.js
+++ tests/lib.test.js
@@ -1,19 +1,30 @@
 const lib = require("./lib");
 
 //Always group related tests inside a describe block
-describe("absolute", () => {
-  it("should return a positive number if input is postive", () => {
-    const result = lib.absolute(1);
-    expect(result).toBe(1);
-  });
+// describe("absolute", () => {
+//   it("should return a positive number if input is postive", () => {
+//     const result = lib.absolute(1);
+//     expect(result).toBe(1);
+//   });
 
-  it("should return a positive number if input is negative", () => {
-    const result = lib.absolute(1);
-    expect(result).toBe(1);
-  });
+//   it("should return a positive number if input is negative", () => {
+//     const result = lib.absolute(1);
+//     expect(result).toBe(1);
+//   });
 
-  it("should return 0 if input is 0", () => {
-    const result = lib.absolute(0);
-    expect(result).toBe(0);
+//   it("should return 0 if input is 0", () => {
+//     const result = lib.absolute(0);
+//     expect(result).toBe(0);
+//   });
+// });
+
+describe('absolute', () => {
+
+});
+
+describe('greet', () => {
+  it('should return the greeting message', () => {
+    const result = lib.greet('Mosh');
+    expect(result).toBe('Welcome Mosh');
   });
 });
