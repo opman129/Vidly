3db526c7883f1f4db74ca1b922504ddc3a26c98f
Jokotagba Opemipo <opemipojokotagba@gmail.com>
2021-01-12 22:37:51 +0100

    installed Jest

--- tests/lib.test.js
+++ tests/lib.test.js
@@ -3,17 +3,17 @@ const lib = require("./lib");
 
 //Always group related tests inside a describe block
 describe("absolute", () => {
-  test("should return a positive number if input is postive", () => {
+  it("should return a positive number if input is postive", () => {
     const result = lib.absolute(1);
     expect(result).toBe(1);
   });
 
-  test("should return a positive number if input is negative", () => {
+  it("should return a positive number if input is negative", () => {
     const result = lib.absolute(1);
     expect(result).toBe(1);
   });
 
-  test("should return 0 if input is 0", () => {
+  it("should return 0 if input is 0", () => {
     const result = lib.absolute(0);
     expect(result).toBe(0);
   });
