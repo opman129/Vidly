7a4db9fc02a95ee904a5c77289b38804cfad5676
Jokotagba Opemipo <opemipojokotagba@gmail.com>
2021-01-13 12:21:43 +0100

    Tests

--- tests/lib.js
+++ tests/lib.js
@@ -13,3 +13,8 @@
 // module.exports.getCurrencies = function() {
 //     return ['USD', 'AUD', 'EUR'];
 // };
+
+//Testing Objects
+module.exports.getProduct = function(productId){
+    return { id: productId, price: 10 };
+}
